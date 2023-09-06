6c6d323c7c4a96b2b4214927b743b85a0be1c4e6
VikVanDik <95vlc17@gmail.com>
2023-09-06 14:25:40 +0200

    [add]: message to script

--- js/script.js
+++ js/script.js
@@ -7,8 +7,10 @@
 const name = prompt ('Qual è il tuo nome?');
 const surname = prompt ('Qual è il tuo cognome?');
 const color = prompt ('Qual è il tuo colore preferito?');
+const number = 23
 
-const message = `ciao $(name), la tua nuova password è $(name)$(surname)$(color)23`
+message = 'Ciao ' + name + '!' + ' La tua nuova password è ' + name + surname + color + number;
 
 console.log(message)
 
+password.innerHTML = message;
